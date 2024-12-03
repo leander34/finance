ad5d2a50578f5a3b50a4bd71580941909e65be79
leander34 <leandersilveira@hotmail.com>
2024-12-03 10:27:15 -0300

    .

--- apps/api/src/infra/http/routes/teste/teste.ts
+++ apps/api/src/infra/http/routes/teste/teste.ts
@@ -18,7 +18,7 @@ export async function teste(app: FastifyInstance) {
     },
     async (request, reply) => {
       return reply.status(200).send({
-        message: 'Olá, Leander dddd do cao 2 gsgsgsjfjfjfgs! gsgsgdadad',
+        message: 'Olá, Leander dddd do cdadadaao 2 gsgsgsjfjfjfgs! gsgsgdadad',
       })
     },
   )
